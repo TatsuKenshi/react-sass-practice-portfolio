3be93ceec15492ad5c4248aa002304e56bc7e7dc
TatsuKenshi <rockinbg@gmail.com>
2022-11-06 21:34:45 +0100

    adds react icons instead of fontawesome, home page structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,72 @@
 import React from "react";
 import "../styling/Home.scss";
+import { NavLink } from "react-router-dom";
+import { FaTwitter, FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <section className="light">
-      <h1>Home</h1>
-      <div className="main">
-        <p className="main-paragraph1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni non
-          fuga natus voluptatum architecto reprehenderit at sit praesentium et
-          libero reiciendis ad, possimus animi assumenda doloribus similique
-          atque nobis distinctio inventore placeat. Mollitia distinctio officia
-          porro qui ipsum sapiente explicabo!
-        </p>
-        <p className="main-paragraph2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni non
-          fuga natus voluptatum architecto reprehenderit at sit praesentium et
-          libero reiciendis ad, possimus animi assumenda doloribus similique
-          atque nobis distinctio inventore placeat. Mollitia distinctio officia
-          porro qui ipsum sapiente explicabo!
-        </p>
-      </div>
-    </section>
+    <>
+      <header>
+        <h1>Hello</h1>
+        <div className="menu-btn">
+          <span className="menu-btn__burger"></span>
+        </div>
+        <nav>
+          <ul className="menu-nav">
+            <li className="menu-nav__item active">
+              <NavLink to="/" className="menu-nav__link">
+                Home
+              </NavLink>
+            </li>
+
+            <li className="menu-nav__item">
+              <NavLink to="/about" className="menu-nav__link">
+                About Me
+              </NavLink>
+            </li>
+
+            <li className="menu-nav__item">
+              <NavLink to="/projects" className="menu-nav__link">
+                My Projects
+              </NavLink>
+            </li>
+
+            <li className="menu-nav__item">
+              <NavLink to="/contact" className="menu-nav__link">
+                Contact Me
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <section className="home">
+          <h2>Hi! My Name Is</h2>
+          <h1 className="home__name">
+            Mike <span className="home__name--last">Smith</span>
+          </h1>
+          <h2>Web Developer, Designer & Programmer</h2>
+
+          <div className="social-icons">
+            <a href="#!">
+              <FaTwitter />
+            </a>
+            <a href="#!">
+              <FaFacebook />
+            </a>
+            <a href="#!">
+              <FaInstagram />
+            </a>
+            <a href="#!">
+              <FaGithub />
+            </a>
+          </div>
+
+          <footer>&copy; Copyright 2022</footer>
+        </section>
+      </main>
+    </>
   );
 };
 
