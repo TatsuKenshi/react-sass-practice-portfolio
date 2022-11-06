0457f4105fe86900cd0ec5c7bf626e5a9244764e
TatsuKenshi <rockinbg@gmail.com>
2022-11-06 19:36:26 +0100

    adds dependencies and tutorial files

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styling/Home.scss";
+
+const Home = () => {
+  return (
+    <section className="light">
+      <h1>Home</h1>
+      <div className="main">
+        <p className="main-paragraph1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni non
+          fuga natus voluptatum architecto reprehenderit at sit praesentium et
+          libero reiciendis ad, possimus animi assumenda doloribus similique
+          atque nobis distinctio inventore placeat. Mollitia distinctio officia
+          porro qui ipsum sapiente explicabo!
+        </p>
+        <p className="main-paragraph2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni non
+          fuga natus voluptatum architecto reprehenderit at sit praesentium et
+          libero reiciendis ad, possimus animi assumenda doloribus similique
+          atque nobis distinctio inventore placeat. Mollitia distinctio officia
+          porro qui ipsum sapiente explicabo!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
